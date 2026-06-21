b8fec3ececb45fadf04d165b025859841cb102ab
priyansh-sinha2109 <priyanshsinha2109@gmail.com>
2026-06-21 18:35:20 +0530

    Phase 3 Completed

--- Cash-flow.js
+++ Cash-flow.js
@@ -8,6 +8,9 @@ const text = document.getElementById("text");
 const amount = document.getElementById("amount");
 const salaryInput = document.getElementById("salary-input");
 const salaryForm = document.getElementById("salary-form");
+const warningBanner = document.getElementById("warning-banner");
+const downloadBtn = document.getElementById("download-report-btn");
+const currencySelect = document.getElementById("currency-select");
 let salary = Number(localStorage.getItem("salary")) || 0;
 
 // Load saved transactions from localStorage
@@ -15,11 +18,13 @@ const localStorageTransactions = JSON.parse(
   localStorage.getItem("transactions"),
 );
 
-// Tracks which transaction is being edited (null = adding new)
+// Tracks which transaction is being edited (null)
 let editId = null;
 let chart;
+let currentRate = 1;
+let currentSymbol = "₹";
 
-// Use saved transactions if they exist, otherwise start with empty array
+// Use saved transactions
 let transactions =
   localStorage.getItem("transactions") !== null ? localStorageTransactions : [];
 
@@ -31,7 +36,6 @@ function generateID() {
 }
 
 //Add Transaction
-// Handles both adding a NEW transaction and saving changes when EDITING (editId !== null)
 function addTransaction(e) {
   e.preventDefault();
 
@@ -45,14 +49,15 @@ function addTransaction(e) {
     return;
   }
 
+  const amountInINR = Number(amount.value) / currentRate;
+
   if (editId !== null) {
-    // Editing existing transaction: replace it with updated values
     transactions = transactions.map((transaction) =>
       transaction.id === editId
         ? {
             ...transaction,
             text: text.value,
-            amount: Number(amount.value),
+            amount: amountInINR,
           }
         : transaction,
     );
@@ -62,7 +67,7 @@ function addTransaction(e) {
     const transaction = {
       id: generateID(),
       text: text.value,
-      amount: Number(amount.value),
+      amount: amountInINR,
     };
 
     transactions.push(transaction);
@@ -86,9 +91,8 @@ salaryForm.addEventListener("submit", function (e) {
     alert("Please enter a valid salary");
     return;
   }
-  salary = Number(salaryInput.value);
 
-  money_plus.innerText = `₹${salary}`;
+  salary = Number(salaryInput.value) / currentRate;
 
   updateValues();
   updateLocalStorage();
@@ -99,13 +103,16 @@ salaryForm.addEventListener("submit", function (e) {
 // ===== READ =====
 
 //Add Trasactions to DOM list
-// Renders a single transaction as a list item on the page
+
 function addTransactionDOM(transaction) {
   const item = document.createElement("li");
 
   //Add Class Based on Value
   item.innerHTML = `
-    ${transaction.text}<span>₹${transaction.amount}</span>
+    ${transaction.text}<span>
+  ${currentSymbol}
+  ${(transaction.amount * currentRate).toFixed(2)}
+</span>
     <button onclick="editTransaction(${transaction.id})">Edit</button>
     <button class="delete-btn" onclick="removeTransaction(${transaction.id})">x</button>
   `;
@@ -113,8 +120,6 @@ function addTransactionDOM(transaction) {
   list.appendChild(item);
 }
 
-//Update the balance income and expence
-// Reads all transactions and recalculates balance, income, expense totals on screen
 function updateValues() {
   const amounts = transactions.map((transaction) => transaction.amount);
 
@@ -122,10 +127,25 @@ function updateValues() {
 
   const remainingBalance = salary - totalExpenses;
 
-  balance.innerText = `₹${remainingBalance.toFixed(2)}`;
-  money_plus.innerText = `₹${salary.toFixed(2)}`;
-  money_minus.innerText = `₹${-totalExpenses.toFixed(2)}`;
-  updateChart(remainingBalance, totalExpenses);
+  const threshold = salary * 0.1;
+
+  if (salary > 0 && remainingBalance < threshold) {
+    balance.style.color = "red";
+
+    warningBanner.style.display = "block";
+  } else {
+    balance.style.color = "";
+
+    warningBanner.style.display = "none";
+  }
+
+  balance.innerText = `${currentSymbol}${(remainingBalance * currentRate).toFixed(2)}`;
+
+  money_plus.innerText = `${currentSymbol}${(salary * currentRate).toFixed(2)}`;
+
+  money_minus.innerText = `${currentSymbol}${(totalExpenses * currentRate).toFixed(2)}`;
+
+  updateChart(remainingBalance * currentRate, totalExpenses * currentRate);
 }
 
 // Update the Chart
@@ -151,7 +171,6 @@ function updateChart(remainingBalance, totalExpenses) {
   });
 }
 
-// Reads transactions from memory and rebuilds the entire list + totals on screen
 function Init() {
   list.innerHTML = "";
 
@@ -159,18 +178,16 @@ function Init() {
   updateValues();
 }
 
-// Run once on page load to display saved data
 Init();
 
 // ===== UPDATE =====
 
 // Edit Expense
-// Fills the form with the selected transaction's data so the user can edit it
 function editTransaction(id) {
   const transaction = transactions.find((transaction) => transaction.id === id);
 
   text.value = transaction.text;
-  amount.value = transaction.amount;
+  amount.value = (transaction.amount * currentRate).toFixed(2);
 
   editId = id; // addTransaction() will now UPDATE this transaction instead of creating a new one
 }
@@ -178,7 +195,6 @@ function editTransaction(id) {
 // ===== DELETE =====
 
 //Remove Transaction by ID
-// Removes a transaction from the list and refreshes the screen
 function removeTransaction(id) {
   transactions = transactions.filter((transaction) => transaction.id !== id);
   updateLocalStorage();
@@ -186,8 +202,104 @@ function removeTransaction(id) {
 }
 
 //update Local Storage Transaction
-// Saves the current salary and transactions list into the browser's localStorage
 function updateLocalStorage() {
   localStorage.setItem("salary", salary);
   localStorage.setItem("transactions", JSON.stringify(transactions));
+}
+
+// Download Expense Report
+downloadBtn.addEventListener("click", downloadReport);
+function downloadReport() {
+  const { jsPDF } = window.jspdf;
+
+  const doc = new jsPDF();
+
+  doc.setFontSize(18);
+  doc.text("Cash Flow Report", 20, 20);
+
+  doc.setFontSize(12);
+
+  const convertedSalary = salary * currentRate;
+  doc.text(`Salary: ${currentSymbol}${convertedSalary.toFixed(2)}`, 20, 40);
+
+  const totalExpenses = transactions.reduce(
+    (acc, transaction) => acc + transaction.amount,
+    0,
+  );
+
+  const remainingBalance = salary - totalExpenses;
+  const convertedExpenses = totalExpenses * currentRate;
+  const convertedBalance = remainingBalance * currentRate;
+
+  doc.text(
+    `Total Expenses: ${currentSymbol}${convertedExpenses.toFixed(2)}`,
+    20,
+    50,
+  );
+
+  doc.text(
+    `Remaining Balance: ${currentSymbol}${convertedBalance.toFixed(2)}`,
+    20,
+    60,
+  );
+
+  doc.text("Expenses:", 20, 80);
+
+  let y = 90;
+
+  transactions.forEach((transaction, index) => {
+    const convertedAmount = transaction.amount * currentRate;
+    doc.text(
+      `${index + 1}. ${transaction.text} - ${currentSymbol}${convertedAmount.toFixed(2)}`,
+      20,
+      y,
+    );
+
+    y += 10;
+  });
+
+  doc.save("CashFlowExpenseReport.pdf");
+}
+
+// Currency Converter
+currencySelect.addEventListener("change", convertCurrency);
+
+const currencySymbols = {
+  INR: "₹",
+  USD: "$",
+  EUR: "€",
+  GBP: "£",
+};
+
+async function convertCurrency() {
+  const selectedCurrency = currencySelect.value;
+
+  currentSymbol = currencySymbols[selectedCurrency] || selectedCurrency;
+
+  if (selectedCurrency === "INR") {
+    currentRate = 1;
+    Init();
+    return;
+  }
+
+  try {
+    const response = await fetch(
+      `https://api.frankfurter.dev/v2/rate/INR/${selectedCurrency}`,
+    );
+
+    if (!response.ok) {
+      throw new Error(`API request failed with status ${response.status}`);
+    }
+
+    const data = await response.json();
+
+    console.log(data);
+
+    currentRate = data.rate;
+
+    Init();
+  } catch (error) {
+    console.error(error);
+    alert("Currency conversion failed");
+  }
 }
